38e5fd607093bd375c343d20dd0ee4205c55e05a
nithu270 <nithyashree.it22@bitsathy.ac.in>
2026-08-09 14:21:37 +0530

    working location

--- my-backend/server.js
+++ my-backend/server.js
@@ -52,7 +52,11 @@ app.post("/api/auth/login", async (req, res) => {
     res.json({ token, userName: user.name });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 const axios = require("axios");
 
 app.get("/api/news", async (req, res) => {
